57d24c02b628201028672f0c68b1f59f1431aaf8
abaduna <artuelrey@gmail.com>
2022-10-18 17:35:45 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { match } from 'assert';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  dadoIxq = "./../assets/img/dice6.png";
+  dadoderecho = "./../assets/img/dice6.png";
+  numero1: Number | undefined  
+  numero2: Number | undefined 
+  tirardados() {
+  this.numero1 = Math.ceil((Math.random()*6))
+ 
+  this.numero2 =Math.ceil((Math.random()*6))
+  console.log(this.numero1)
+  console.log(this.numero2)
+  this.dadoIxq = "./../assets/img/dice"+ this.numero1 +".png"
+  this.dadoderecho = "./../assets/img/dice"+ this.numero2 +".png"
+  
+}
   title = 'dados';
+  
 }
